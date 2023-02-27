2b92689a229f0b119de538101000fac2250e1c2b
Edwin Ryerson <eryerson06@gmail.com>
2023-02-27 09:43:53 -0700

    Create Student Constructor

--- Student.java
+++ Student.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class Student extends User {
-    public Student() {}
+    
+    private Book checkedOutBook;
+    private ArrayList<Book> pastCheckedOutBooks;
+
+    public Student(String username) {
+        super(username);
+        this.checkedOutBook = null;
+        this.pastCheckedOutBooks = new ArrayList<Book>();
+    }
+
+    
 }
